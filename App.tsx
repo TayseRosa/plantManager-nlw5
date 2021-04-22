e707a020ca0056e3727bcb0640d32db5ed741b68
Tayse Rosa <tayse_rosa@yahoo.com.br>
2021-04-22 08:51:07 -0300

    2º e 3 aula da NLW

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import{ View, Text, StyleSheet } from 'react-native';
-import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading';
+import Routes from './src/routes';
+
+import { 
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+ } from '@expo-google-fonts/jost';
+
+import Confirmation from './src/pages/Confirmation';
+// import 'react-native-gesture-handler';
 
 export default function App(){
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+
+  return <AppLoading />
+    
+
   return(
-      <Welcome />
-  );
+      <Routes />
+  )
 }
